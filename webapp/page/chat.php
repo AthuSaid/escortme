0cd8db68887ddaaadb9604908813b97522b10dc5
schokobaer <andreas.peter.fend@gmail.com>
2017-02-18 11:00:11 +0100

    chat.php: input container init (problem with textarea size)

---
+++ webapp/page/chat.php
@@ -0,0 +1,55 @@
+<div class="esc-input-ct">
+  <div class="esc-input-msg">
+    <textarea rows="1" placeholder="Schreibe etwas ..."></textarea>
+  </div>
+  <div class="esc-input-send">
+    <div class="esc-input-send-btn">
+      <img src="img/send.png" />
+    </div>
+  </div>
+</div>
+
+
+<style type="text/css">
+  .esc-input-ct{
+    position: absolute;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    background-color: #3498DB;
+    padding: 5px 0.5cm;
+    display: flex;
+    align-items: center;
+  }
+  .esc-input-msg{
+    width: 100%;
+    display: flex;
+  }
+  .esc-input-msg textarea{
+    width: 100%;
+    padding-left: 5px;
+    padding-right: 5px;
+    border-radius: 5px;
+  }
+  .esc-input-send{
+    margin-left: 10px;
+  }
+  .esc-input-send-btn{
+    background-color: #2980B9;
+    height: 1cm !important;
+    width: 1cm !important;
+    border-radius: 1cm;
+    padding: 0.18cm 0.28cm;
+  }
+  .esc-input-send-btn:active{
+    background-color: #E74C3C;
+  }
+  .esc-input-send-btn img{
+    height: 0.5cm;
+    width: 0.5cm;
+  }
+</style>
+
+<script type="text/javascript">
+  
+</script>
