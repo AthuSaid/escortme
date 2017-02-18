9583f3c8e21347192829b95e33f4bad33da61f7a
schokobaer <andreas.peter.fend@gmail.com>
2017-02-18 12:22:45 +0100

    chat.php: messages (without time) finished. no functionality yet

--- webapp/page/chat.php
+++ webapp/page/chat.php
@@ -1,3 +1,103 @@
+<div class="esc-msg-ct">
+
+  <div class="esc-msg-right">
+    <div class="esc-msg">
+      <div class="esc-msg-avatar">
+        <img src="data/profil-3.jpg" />
+      </div>
+      <div class="esc-msg-content">
+        Hallo Hübsche, bist du heute zu haben?
+      </div>
+    </div>
+  </div>
+
+  <div class="esc-msg-left">
+    <div class="esc-msg">
+      <div class="esc-msg-avatar">
+        <img src="data/profil-4.jpg" />
+      </div>
+      <div class="esc-msg-content">
+        Hallo du ;)<br />
+        Was hättest du denn gerne?
+      </div>
+    </div>
+  </div>
+
+  <div class="esc-msg-right">
+    <div class="esc-msg">
+      <div class="esc-msg-avatar">
+        <img src="data/profil-3.jpg" />
+      </div>
+      <div class="esc-msg-content">
+        Ich hab ab 8 Uhr Zeit und bin leider ganz alleine heut :(
+        Ich bräucht mal wieder nen Blowjob!
+      </div>
+    </div>
+  </div>
+
+  <div class="esc-msg-left">
+    <div class="esc-msg">
+      <div class="esc-msg-avatar">
+        <img src="data/profil-4.jpg" />
+      </div>
+      <div class="esc-msg-content">
+        Ich kann ab 9 Uhr. Am liebsten wärs mir, wenn du zu mir kommst.
+        Bezahlung ist bei mir immer Bar! Ich hab Kondome da wenn du
+        ficken willst. Das kannst du dann ja noch spontan schauen ;)
+      </div>
+    </div>
+  </div>
+
+  <div class="esc-msg-right">
+    <div class="esc-msg">
+      <div class="esc-msg-avatar">
+        <img src="data/profil-3.jpg" />
+      </div>
+      <div class="esc-msg-content">
+        Hallo Hübsche, bist du heute zu haben?
+      </div>
+    </div>
+  </div>
+
+  <div class="esc-msg-left">
+    <div class="esc-msg">
+      <div class="esc-msg-avatar">
+        <img src="data/profil-4.jpg" />
+      </div>
+      <div class="esc-msg-content">
+        Hallo du ;)<br />
+        Was hättest du denn gerne?
+      </div>
+    </div>
+  </div>
+
+  <div class="esc-msg-right">
+    <div class="esc-msg">
+      <div class="esc-msg-avatar">
+        <img src="data/profil-3.jpg" />
+      </div>
+      <div class="esc-msg-content">
+        Ich hab ab 8 Uhr Zeit und bin leider ganz alleine heut :(
+        Ich bräucht mal wieder nen Blowjob!
+      </div>
+    </div>
+  </div>
+
+  <div class="esc-msg-left">
+    <div class="esc-msg">
+      <div class="esc-msg-avatar">
+        <img src="data/profil-4.jpg" />
+      </div>
+      <div class="esc-msg-content">
+        Ich kann ab 9 Uhr. Am liebsten wärs mir, wenn du zu mir kommst.
+        Bezahlung ist bei mir immer Bar! Ich hab Kondome da wenn du
+        ficken willst. Das kannst du dann ja noch spontan schauen ;)
+      </div>
+    </div>
+  </div>
+
+</div>
+
 <div class="esc-input-ct">
   <div class="esc-input-msg">
     <textarea rows="1" placeholder="Schreibe etwas ..."></textarea>
@@ -12,7 +112,7 @@
 
 <style type="text/css">
   .esc-input-ct{
-    position: absolute;
+    position: fixed;
     bottom: 0px;
     left: 0px;
     right: 0px;
@@ -48,8 +148,57 @@
     height: 0.5cm;
     width: 0.5cm;
   }
+
+  .esc-msg-ct{
+    padding: 10px 0.5cm 10px 0.5cm;
+    margin-bottom: 1cm;
+  }
+  .esc-msg-ct > div{
+    margin-bottom: 10px;
+  }
+
+  .esc-msg{
+    max-width: 90%;
+    color: #ECF0F1;
+    font-family: Roboto-Light;
+    font-size: 12px;
+    line-height: 1.2;
+    display: inline-flex;
+    justify-content: space-between;
+    border-radius: 10px;
+    padding: 5px;
+  }
+  .esc-msg-left > div{
+    background-color: #3498DB;
+  }
+  .esc-msg-right > div{
+    background-color: #4caf50;
+    flex-direction: row-reverse;
+  }
+  .esc-msg-right .esc-msg-avatar{
+    margin-right: 0px;
+    margin-left: 5px;
+  }
+  .esc-msg-right{
+    display: flex;
+    justify-content: flex-end;
+  }
+  .esc-msg-avatar{
+    margin-right: 5px;
+    min-height: 0.8cm;
+  }
+  .esc-msg-avatar img{
+    height: 0.8cm;
+    width: 0.8cm;
+    border-radius: 0.5cm;
+  }
+  .esc-msg-content{
+    padding-left: 3px;
+    padding-right: 3px;
+  }
 </style>
 
 <script type="text/javascript">
-  
+  Topbar.show();
+  Topbar.setText("Marina");
 </script>
